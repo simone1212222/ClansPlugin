2754441069663cfa34b40eba8e75b9b37adbf848
Simone <xusimone51@gmail.com>
2025-08-03 02:50:18 +0200

    Initial commit

---
+++ src/main/java/com/develop/clansPlugin/ClansPlugin.java
@@ -0,0 +1,17 @@
+package com.develop.clansPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ClansPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
